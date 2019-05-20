7b627c9bdeb61772e8f6b84cb47497fcbb903abc
Anushka2600 <50695970+Anushka2600@users.noreply.github.com>
2019-05-20 21:45:53 +0530

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,35 +1,39 @@
-// Bubble sort
+
 import java.util.*;
+public class Solution
+{
+ public static void main(String[] args) 
+ {
+   int arr[] = { 10, 7, 3, 1, 9, 7, 4, 3 };
+   System.out.print("Initial Array : ");
+   printArray(arr);
+   bubbleSort(arr);
+  }
+  public static void printArray(int[] arr)
+  {
+  for(int m = 0; m < arr.length; m++)
+  {
+   System.out.print(arr[m] + " ");
+   }
 
-public class Solution {
+  System.out.println();
+  }
+  static void bubbleSort(int[] arr)
+  {
+  for (int m = 0; m < arr.length; m++) 
+  {
+   for (int n = 0; n < arr.length - m - 1; n++)
+   {
+   if (arr[n] > arr[n + 1]) 
+   {
+    int temp = arr[n];
+    arr[n] = arr[n + 1];
+    arr[n + 1] = temp;
+    }
+    }
 
-  public static void main(String[] args) {
-    // Initialize arr
-    int arr[] = { 10, 7, 3, 1, 9, 7, 4, 3 };
-    System.out.print("Initial Array : ");
+    System.out.print("After pass " + m + "  : ");
     printArray(arr);
-    bubbleSort(arr);
   }
-  public static void printArray(int[] arr) {
-    for(int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
-    }
-
-    System.out.println();
   }
-  static void bubbleSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr.length - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          int temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
-      }
-
-      System.out.print("After pass " + i + "  : ");
-      //Printing array after pass
-      printArray(arr);
-    }
   }
-}
